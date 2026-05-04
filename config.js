5f05a113abb0805ce338304f192d1c6702c3dff4
chokoranb2 <chokoranb2@icloud.com>
2026-05-05 06:08:08 +0900

    Initial hazard tank dashboard

---
+++ config.js
@@ -0,0 +1,7 @@
+window.DASHBOARD_CONFIG = {
+  SUPABASE_URL: "https://modjfdtgkxkvxdcffcyx.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_IHKNNw2t8AWE2yKyPX8sSA_W11U4wHU",
+  TABLE_NAME: "hazard_tank_telemetry",
+  REFRESH_INTERVAL_MS: 300000,
+  STALE_MINUTES: 15
+};
